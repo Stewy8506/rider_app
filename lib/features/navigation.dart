46db6859b61bdcd2db2178eba405ba9ec334d695
Anuvab Das <dasanuvab38@gmail.com>
2026-04-14 12:03:59 +0530

    feat: update dependencies and add macOS support
    - Added new dependencies for Supabase, Dio, Riverpod, Intl, Google Maps, Geolocator, Geocoding, WebSocket, Spotify SDK, Local Notifications, UUID, and Cached Network Image in pubspec.yaml.
    - Registered new plugins (AppLinks, Geolocator, UrlLauncher) in generated_plugin_registrant.cc for Windows.
    - Updated generated_plugins.cmake to include new plugins for Windows.
    - Created a new Podfile for macOS support, including necessary configurations and dependencies for Flutter.

---
+++ lib/features/navigation.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class NavigationWidget extends StatefulWidget {
+  NavigationWidget({super.key});
+  int _selectedIndex = 0;
+  bool _isSwiping = false;
+
+  @override
+  State<NavigationWidget> createState() => _NavigationWidgetState();
+}
+
+class _NavigationWidgetState extends State<NavigationWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Selected Index: ${widget._selectedIndex}'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: widget._selectedIndex,
+        onTap: (index) {
+          setState(() {
+            widget._selectedIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
